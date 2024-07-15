762db01bc0e4f67ec8644dd9632a4096d48e782c
software@dijitex <software@dijitex.in>
2024-07-16 01:41:54 +0530

    fixed pydantic error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 import tempfile
-uploadedFile: tempfile
+# uploadedFile: tempfile
 
 
 app = FastAPI()
@@ -10,13 +10,13 @@ app = FastAPI()
 def get_latest_version():
     return {"version" : "1.0"}
 
-@app.post("/uploadFile")
-def upload_file(file: UploadFile):
-    print(file.file.read())
-    print("------> file format",file.filename.format)
-    # uploadedFile = tempfile(file.filename, file.file)
-    return {"filename" : file.filename,
-            "success" : "true" }
+# @app.post("/uploadFile")
+# def upload_file(file: UploadFile):
+#     print(file.file.read())
+#     print("------> file format",file.filename.format)
+#     # uploadedFile = tempfile(file.filename, file.file)
+#     return {"filename" : file.filename,
+#             "success" : "true" }
 
 # @app.get("/getFile")
 # def get_file():
@@ -24,4 +24,5 @@ def upload_file(file: UploadFile):
 
 @app.post("/downloadFile")
 async def download_file():
+    print("------> file sent")
     return FileResponse("IW317-WSEN_3_V01.00.ufw")
